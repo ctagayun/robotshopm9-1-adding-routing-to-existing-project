202ff1ead1cbb0510e5241942b30606411fdaa14
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-05 16:19:23 -0600

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,14 @@ const routes: Routes = [];
   declarations: [],
   imports: [
     CommonModule, // no need because app.module imorts it already
-    RouterModule //add this
+    RouterModule.forRoot(routes) //add this. this syntax allows us to pass our "routes"
+                                 //array to the router. forRoot because we are providing the
+                                 //routes to our application.
+                                 //Contrast that with forChild. We will use this later on
+                                 //when we create a child modules with its own routes
+
+                                 //The last thing to do is add a <router-outlet> in
+                                 //AppComponent
   ],
   exports: [RouterModule] //add this
 })
